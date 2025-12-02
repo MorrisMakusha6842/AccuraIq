b230f888d03a38f05aac2f12e775a86a0cf22737
8rown <8rown-email@example.com>
2025-12-02 09:39:45 +0200

    wrapping components

---
+++ src/app/general-ledger/general-ledger.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-ledger',
+  imports: [],
+  templateUrl: './general-ledger.component.html',
+  styleUrl: './general-ledger.component.scss'
+})
+export class GeneralLedgerComponent {
+
+}
